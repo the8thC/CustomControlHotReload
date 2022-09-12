734d0e956820fd5635d5aaa0dfd13b4316ffc45c
eugene gusev <the8thc@yandex.ru>
2022-09-12 17:57:12 +0400

    init

---
+++ MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace CustomControlHotReload;
+
+public class MyTemplatedControl : VisualElement
+{
+    public static readonly BindableProperty ItemTemplateProperty =
+        BindableProperty.Create(nameof(ItemTemplate), typeof(DataTemplate), typeof(MyTemplatedControl), null, propertyChanged: OnItemTemplateChanged);
+
+    public DataTemplate ItemTemplate {
+        get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+        set { SetValue(ItemTemplateProperty, value); }
+    }
+
+    public VisualElement Item { get; set; }
+
+    static void OnItemTemplateChanged(BindableObject bindable, object oldValue, object newValue) {
+        Console.WriteLine($"ItemTemplate changed from {oldValue} to {newValue}!");
+    }
+}
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        this.listView.ItemsSource = new List<string>() {
+            "Item1",
+        };
+    }
+
+    void Grid_PropertyChanged(System.Object sender, System.ComponentModel.PropertyChangedEventArgs e) {
+        Console.WriteLine($"Property {e.PropertyName} changed!");
+    }
+
+    void Button_Clicked(System.Object sender, System.EventArgs e) {
+        ChangeMyItemBackgroundColor(this.myTemplatedControl.Item);
+    }
+
+    void ChangeMyItemBackgroundColor(VisualElement visualElement) {
+        if (visualElement == null)
+            return;
+
+        visualElement.BackgroundColor = visualElement.BackgroundColor == Colors.BlueViolet ?
+                                        Colors.Red : Colors.BlueViolet;
+    }
+}
+
---
+++ MauiProgram.cs
@@ -0,0 +1,26 @@
+﻿#if ANDROID
+using CustomControlHotReload.Platforms.Android;
+#endif
+
+namespace CustomControlHotReload;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                }).ConfigureMauiHandlers(handlers => {
+#if ANDROID
+                    handlers.AddHandler(typeof(MyTemplatedControl), typeof(MyTemplatedControlHandler));
+#endif
+                });
+
+		return builder.Build();
+	}
+}
